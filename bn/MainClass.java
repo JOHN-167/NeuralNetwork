a8454c9222523d741af2e7627fbfacd4d7474b23
John Nguyen <nguyenduy167@yahoo.com>
2023-11-23 16:15:12 -0500

    up

--- bn/MainClass.java
+++ bn/MainClass.java
@@ -14,18 +14,7 @@ import bn.src.simulator.Simulator;
 public class MainClass {
     
     public static void main(String[] args) throws FileNotFoundException {
-
-        /**
-         * java MainClass filepath inputsize outputsize trainpercent type trials threshold
-         * 
-         * - inputsize (int): the size of the input
-         * - outputsize (int): the size of the output
-         * - trainpercent (double): the percentage of the data set used for training ([0,1])
-         * - type (int): 1 (Linear Perceptron), 2 (Logistic Perceptron), 3 (SingleLayered Perceptron)
-         * - trials (int): the number of times the model is trained
-         * - threshold (double): for models that require a threshold, this is the threshold
-         */
-        
+        // create file
         File file = new File(args[0]);
         
         // create the reader and read the file
